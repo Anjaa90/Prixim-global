ab3ae299dc5e666048914bde83a7b284b8885040
Avinashkdm10 <avinash.prixim@gmail.com>
2025-01-07 13:28:20 +0530

    chasnge

--- includes/header.php
+++ includes/header.php
@@ -45,8 +45,9 @@
             <ul>
               <li><img src="assets/img/icon/mail.png" alt="" loading="lazy"><a
                   href="mailto:sales@priximglobal.com">sales@priximglobal.com</a></li>
-              <li><img src="assets/img/icon/phone-call.webp" alt="united kingdom" loading="lazy" /><a
-                  href="tel:+971544335310">+971544335310</a></li>
+              <li><img src="assets/img/whatsapp.webp" alt="united kingdom" loading="lazy" /><a
+                  href="https://wa.me/+917972359229">+971544335310</a></li>
+              <li><img src="assets/img/skype.webp" alt=""><a href="https://join.skype.com/invite/xraWIPh4p9eP" target="_blank"> Prixim Global</a></li>
 
             </ul>
           </div>
