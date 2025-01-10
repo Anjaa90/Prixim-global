90da29b125f0bad2906c61fe5a28a3c970c20a87
Avinashkdm10 <avinash.prixim@gmail.com>
2025-01-10 12:08:50 +0530

    chasnge

--- payments-psp.php
+++ payments-psp.php
@@ -207,7 +207,8 @@
             </div>
         </div>
     </main>
-    <footer class="footer-area">
+
+    <!-- <footer class="footer-area">
         <div class="footer-widget">
             <div class="container">
                 <div class="footer-top pt-40 pb-40">
@@ -401,7 +402,72 @@
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> -->
+    <footer class="footer-area">
+         <div class="floating-button"> 
+            <a href="https://wa.me/971544335310" class="whatsapp-button">
+                <img src="assets/img/icon/whatsapp.webp" alt="WhatsApp" loading="lazy">
+            </a>
+            <div class="text-bubble">
+                Say Hello to Prixim Global<br> How may I help you?
+            </div>
+        </div>
+        <script src="script.js"></script>
+        <style>
+           .floating-button {
+                position: fixed;
+                bottom: 90px;
+                left: 20px;
+                display: flex;
+                align-items: center;
+                gap: 10px;
+                z-index: 9999;
+                /* Very high value to ensure it stays above other elements */
+            }
+
+            .text-bubble {
+                background-color: white;
+                border-radius: 10px;
+                padding: 10px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+                opacity: 0;
+                transform: translateX(20px);
+                animation: fadeInText 1s forwards;
+                animation-delay: 0.5s;
+                font-size: 14px;
+            }
+
+            @keyframes fadeInText {
+                0% {
+                    opacity: 0;
+                    transform: translateX(20px);
+                }
+
+                100% {
+                    opacity: 1;
+                    transform: translateX(0);
+                }
+            }
+
+            .whatsapp-button {
+                background-color: #25D366;
+                border-radius: 50%;
+                padding: 15px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+            }
+
+            .whatsapp-button img {
+                width: 30px;
+                height: 30px;
+            }
+            @media (max-width: 750px) {
+            .floating-button {   
+                bottom: 160px;
+            }
+            }
+        </style>
+
+<?php include ('includes/footer.php');?>
     <script
         type="text/javascript">var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date(); (function () { var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0]; s1.async = true; s1.src = 'https://embed.tawk.to/645b4eb9ad80445890ec1d03/1h02an22m'; s1.charset = 'UTF-8'; s1.setAttribute('crossorigin', '*'); s0.parentNode.insertBefore(s1, s0); })(); </script>
     <a href="#" id="scroll-top"><i class="far fa-arrow-up-to-line"></i></a>
